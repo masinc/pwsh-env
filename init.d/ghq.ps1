c25e8996d7386471e2c21ef5c743eeb046208f30
masinc <masinc000@gmail.com>
2022-03-10 01:29:53 +0900

    add ghq

---
+++ init.d/ghq.ps1
@@ -0,0 +1,14 @@
+Get-Command ghq >$null
+
+if ($?) {
+    function Select-Ghq-Repository {
+        "$(ghq root)\$((ghq list) -replace "/", "\"  | fzf --preview 'ghq show -p {1}')"
+    }
+
+    function __ghq__cd__ {
+        Set-Location "$(Select-Ghq-Repository)"
+    }
+
+    Set-Alias ghq-select Select-Ghq-Repository
+    Set-Alias ghq-cd __ghq__cd__
+}
