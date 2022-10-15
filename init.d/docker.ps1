38efc2e72112032d60616c4e7638e191266bd3dd
masinc <masinc000@gmail.com>
2022-10-15 21:53:02 +0900

    module installation during install-deps

--- init.d/docker.ps1
+++ init.d/docker.ps1
@@ -2,8 +2,4 @@ if (-not (Test-Command docker)) {
     return
 }
 
-if (-not (Test-Module DockerCompletion)) {
-    Install-Module DockerCompletion
-}
-
 Import-Module DockerCompletion
--- init.d/git.ps1
+++ init.d/git.ps1
@@ -2,9 +2,4 @@ if (-not (Test-Command git)) {
     return
 }
 
-if (-not (Test-Module posh-git)) {
-    Install-Module posh-git
-}
-
 Import-Module posh-git
-
