0e215c9016cc4f6b103c8e0b4a529c0c8a7d5fc9
masinc <masinc000@gmail.com>
2022-11-23 12:07:34 +0900

    add pcsx and refacor

--- install-deps.ps1
+++ install-deps.ps1
@@ -1,1 +1,4 @@
-Install-Module DockerCompletion
+Install-Module DockerCompletion -Scope CurrentUser
+Install-Module posh-git -Scope CurrentUser
+Install-Module VSSetup -Scope CurrentUser
+Install-Module Pscx -Scope CurrentUser -AllowClobber 
