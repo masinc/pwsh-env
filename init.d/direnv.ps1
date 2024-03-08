6d5a88f89af009b6983c2d9dfde9b79b8a2c0321
masinc <masinc000@gmail.com>
2024-03-09 00:53:27 +0900

    add comment

--- init.d/direnv.ps1
+++ init.d/direnv.ps1
@@ -2,6 +2,7 @@ if (-not (Test-Command direnv)) {
     return
 }
 
+# https://github.com/direnv/direnv/issues/1105
 $env:DIRENV_CONFIG = "$APPDATA\direnv"
 $env:XDG_CACHE_HOME = "$APPDATA\direnv\cache"
 $env:XDG_DATA_HOME = "$APPDATA\direnv\data"
